aeddd171864fc4b118ccef55370311e9c433a2b8
Usuario <usuario@MacBookPro.lan>
2026-02-02 15:20:49 -0500

    update  search icon

--- app/components/Header/MainNav.tsx
+++ app/components/Header/MainNav.tsx
@@ -70,7 +70,7 @@ const MainNav: FC<Props> = ({className = '', isHome, brands = [], quickLinks}) =
             >
               <div className="relative">
                 <span className="bg-black rounded-full text-white absolute left-2 top-1/2 -translate-y-1/2 text-black">
-                  <MagnifyingGlassIcon />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32" fill="none"><title>Icon</title><path stroke="#252525" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" d="M28 27.9999L22.2 22.1999M25.3333 14.6667C25.3333 20.5577 20.5577 25.3333 14.6667 25.3333C8.77563 25.3333 4 20.5577 4 14.6667C4 8.77563 8.77563 4 14.6667 4C20.5577 4 25.3333 8.77563 25.3333 14.6667Z"></path></svg>
                 </span>
                 <input
                   type="search"
